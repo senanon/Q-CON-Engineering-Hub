e0bbfaa0701609a704d652cc1d4b93f87c55f33c
senanon <38965602+senanon@users.noreply.github.com>
2026-06-22 22:07:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function openProject(){
+
+    alert("Project Management");
+
+}
+
+function openProgress(){
+
+    alert("Progress Update");
+
+}
+
+function openDocuments(){
+
+    alert("Document Center");
+
+}
+
+function openDashboard(){
+
+    alert("Power BI Dashboard");
+
+}
